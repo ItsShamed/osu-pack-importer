1ef2ed1fae4a8ab3bda2a978f98fc4114707055b
Ryuki <thedjplays.gaming@gmail.com>
2022-03-13 23:15:26 +0100

    add: Implemented CollectionDB parsing and serialization

--- OsuPackImporter/Collections/CollectionDB.cs
+++ OsuPackImporter/Collections/CollectionDB.cs
@@ -1,20 +1,70 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using OsuPackImporter.Interfaces.Parsers;
 using OsuPackImporter.Interfaces.Serializers;
 
 namespace OsuPackImporter.Collections
 {
-
     public class CollectionDB : IParsable, ISerializable
     {
-
         private int _version;
-        private List<Collection> _collections;
+        private Stream _fileStream;
+
+        public int Version => _version;
+        public List<Collection> Collections { get; }
+
+        public CollectionDB(Stream stream)
+        {
+            Collections = new List<Collection>();
+            _fileStream = stream;
+            Parse();
+        }
+
+        public CollectionDB(string path) : this(File.OpenRead(path))
+        {
+        }
+
+        ~CollectionDB()
+        {
+            _fileStream.Dispose();
+        }
 
         public IParsable Parse()
         {
-            throw new System.NotImplementedException();
+            try
+            {
+                using (BinaryReader reader = new BinaryReader(_fileStream))
+                {
+                    _version = reader.ReadInt32();
+                    int iterations = reader.ReadInt32();
+                    Console.WriteLine(iterations + " iterations");
+                    for (int i = 0; i < iterations; i++)
+                    {
+                        if (reader.ReadByte() != 0x0b) throw new FormatException("Invalid string");
+                        LegacyCollection collection = new LegacyCollection(reader.ReadString());
+                        Console.WriteLine(collection.Name);
+                        int beatmapIterations = reader.ReadInt32();
+                        Console.WriteLine(beatmapIterations + " beatmap iterations");
+                        for (int j = 0; j < beatmapIterations; j++)
+                        {
+                            if (reader.ReadByte() != 0x0b) throw new FormatException("Invalid string");
+                            string hex = reader.ReadString();
+                            Console.WriteLine(hex);
+                            collection.BeatmapHashes.Add(StringToByteArray(hex));
+                        }
+
+                        Collections.Add(collection);
+                    }
+                }
+
+                return this;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return this;
+            }
         }
 
         public byte[] Serialize()
@@ -24,8 +74,8 @@ namespace OsuPackImporter.Collections
                 using (BinaryWriter writer = new BinaryWriter(memstream))
                 {
                     writer.Write(_version);
-                    writer.Write(_collections.Count);
-                    foreach (Collection collection in _collections)
+                    writer.Write(Collections.Count);
+                    foreach (Collection collection in Collections)
                     {
                         writer.Write(collection.Serialize());
                     }
@@ -33,6 +83,15 @@ namespace OsuPackImporter.Collections
 
                 return memstream.ToArray();
             }
+        }
+
+        private byte[] StringToByteArray(string hex)
+        {
+            int charCount = hex.Length;
+            byte[] bytes = new byte[charCount / 2];
+            for (int i = 0; i < charCount; i += 2)
+                bytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
+            return bytes;
         }
     }
 }
