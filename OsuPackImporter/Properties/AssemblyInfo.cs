129410545cdb9c291c3cddca0b4ad43cf182b275
Ryuki <thedjplays.gaming@gmail.com>
2022-06-13 23:34:28 +0200

    add: setup automatic versionning (and remove Rider AssemblyInfo defaults)

--- OsuPackImporter/Properties/AssemblyInfo.cs
+++ OsuPackImporter/Properties/AssemblyInfo.cs
@@ -4,14 +4,14 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OsuPackImporter")]
-[assembly: AssemblyDescription("Import osu! beatmap packs as collections in the game")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Kirutsu")]
-[assembly: AssemblyProduct("OsuPackImporter")]
-[assembly: AssemblyCopyright("Copyright © Kirutsu 2022. Licensed under the MIT license")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+// [assembly: AssemblyTitle("OsuPackImporter")]
+// [assembly: AssemblyDescription("Import osu! beatmap packs as collections in the game")]
+// [assembly: AssemblyConfiguration("")]
+// [assembly: AssemblyCompany("Kirutsu")]
+// [assembly: AssemblyProduct("OsuPackImporter")]
+// [assembly: AssemblyCopyright("Copyright © Kirutsu 2022. Licensed under the MIT license")]
+// [assembly: AssemblyTrademark("")]
+// [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0")]
-[assembly: AssemblyFileVersion("0.1.0")]
+// [assembly: AssemblyVersion("0.1.0")]
+// [assembly: AssemblyFileVersion("0.1.0")]
